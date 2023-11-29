e87cefc67d0bf28639e34ca19e34552c725d9f0a
AxelZublena <zubl0001@hz.nl>
2023-11-29 09:52:10 +0100

    Attempt to fix CORS issue

--- app/main.py
+++ app/main.py
@@ -5,17 +5,17 @@ from app.api.api_v1.api import router as api_router
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost:5173/",
-#     "https://test.axelzublena.com/"
-# ]
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://test.axelzublena.com",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET, POST, OPTIONS, DELETE, PUT, UPDATE"],
     allow_headers=["*"],
 )
 
