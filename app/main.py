04f186792051de48a40527a5dd8a72cabe4772d3
AxelZublena <zubl0001@hz.nl>
2023-11-29 10:41:05 +0100

    Attempt to fix CORS policy

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "https://test.axelzublena.com",
 ]
 
